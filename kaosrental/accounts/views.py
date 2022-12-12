52414a80712c8cab9edb2ba3e7df0f1c5f2350fc
ekrrkaan <88788988+ekrrkaan@users.noreply.github.com>
2022-12-12 19:02:48 +0300

    Add files via upload

--- kaosrental/accounts/views.py
+++ kaosrental/accounts/views.py
@@ -1,11 +1,61 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import *
-from .forms import RentForm
+from .forms import RentForm , CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
+
+
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     rent = Rent.objects.all()
     customers = Customer.objects.all()
@@ -23,11 +73,13 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
 
     return render(request, 'accounts/products.html', {'products':products})
 
+@login_required(login_url='login')
 def customer(request, pk_test): 
     customer = Customer.objects.get(id=pk_test)
 
@@ -37,6 +89,7 @@ def customer(request, pk_test):
     context = {'customer':customer, 'Rent':Rent, 'rent_count':rent_count}
     return render(request, 'accounts/customer.html',context)
 
+@login_required(login_url='login')
 def createRent(request, pk):
     customer = Customer.objects.get(id=pk)
     form = RentForm(initial={'customer':customer})
@@ -52,6 +105,7 @@ def createRent(request, pk):
     context = {'form':form}
     return render(request, 'accounts/rent_form.html', context)
 
+@login_required(login_url='login')
 def updateRent(request, pk):
 
     rent = Rent.objects.get(id=pk)
@@ -67,6 +121,7 @@ def updateRent(request, pk):
     context = {'form':form}
     return render(request, 'accounts/rent_form.html', context)
 
+@login_required(login_url='login')
 def deleteRent(request, pk):
     rent = Rent.objects.get(id=pk)
     if request.method == "POST":
@@ -75,5 +130,4 @@ def deleteRent(request, pk):
 
     context = {'item':rent}
     return render(request, 'accounts/delete.html', context)
-
 
--- kaosrental/kaosrental/settings.py
+++ kaosrental/kaosrental/settings.py
@@ -12,17 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-32450&&_w@s-mzn%-$$9@cgs^0b)7%_@q0j12-t!x0q=1-!hn7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
